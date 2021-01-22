b8ec236240490b50b19f15cc63f16fbfa7d6c25d
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-01-23 00:25:06 +0100

    Add routes to App.js- missing views

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Regions from './components/views/Regions/RegionsContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
+
 
 class App extends React.Component {
   static propTypes = {
@@ -40,7 +45,11 @@ class App extends React.Component {
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
+            <Route exact path='/trip/:id' component={Trip} />
             {/* TODO - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
