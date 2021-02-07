026c21841e7ea86ea3098ff2ab2e20f045858787
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-07 23:22:37 +0100

    Add test 5

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -4,6 +4,14 @@ import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
 
+  constructor(){
+    super();
+
+    /* run this.forceUpdate() every second */
+    setTimeout(() =>
+      this.forceUpdate(), 1000);
+  }
+
   getCountdownTime() {
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
