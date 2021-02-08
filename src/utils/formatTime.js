5cdd0f89db92db7c07934567d402b14e731e7071
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-08 22:46:10 +0100

    Update formatTime

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,6 +1,14 @@
-
-export const formatTime = (time) => {
-  if(typeof time  !== 'number' || time < 0){
+export const formatTime = (seconds) => {
+  if (typeof seconds !== 'number' || seconds < 0) {
     return null;
+  } else {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds / 60) % 60);
+    let sec = Math.floor(seconds % 60);
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+    sec = sec.toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${sec}`;
   }
 };
